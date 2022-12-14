0442d7b50948f75bea78ab9ee181a0bf23f16d2b
li chenhui <lichenhui821@qq.com>
2022-12-15 04:03:41 +0800

    feat: 文章列表分页加载

--- src/hooks/useMountData.ts
+++ src/hooks/useMountData.ts
@@ -1,16 +1,90 @@
 import axios from "axios"
-import { useEffect, useState } from "react"
+import { useEffect, useState, useRef } from "react"
 import { ArticleType } from "../views/Articles/types"
 
-function useMountData () {
+function useMountData (pageIndex: number) {
   const [list, setList] = useState<ArticleType[]>([])
+  const hasMore = useRef(true)
   useEffect(() => {
-    axios.get('/api/articles/articles').then(res => {
-      setList(res.data.data.list)
+    console.log('useMountData')
+    axios.get(`/api/articles/articles?pageIndex=${pageIndex}`).then(res => {
+      setList([...res.data.data.list, {
+        id: 15,
+        tag: 'a',
+        title: 'xxx',
+        desc: '222',
+        updatedAt: '2222',
+        createdAt: '3333'
+      }, {
+        id: 5,
+        tag: 'a',
+        title: 'xxx',
+        desc: '222',
+        updatedAt: '2222',
+        createdAt: '3333'
+      }, {
+        id: 6,
+        tag: 'a',
+        title: 'xxx',
+        desc: '222',
+        updatedAt: '2222',
+        createdAt: '3333'
+      }, {
+        id: 7,
+        tag: 'a',
+        title: 'xxx',
+        desc: '222',
+        updatedAt: '2222',
+        createdAt: '3333'
+      }, {
+        id: 8,
+        tag: 'a',
+        title: 'xxx',
+        desc: '222',
+        updatedAt: '2222',
+        createdAt: '3333'
+      }, {
+        id: 9,
+        tag: 'a',
+        title: 'xxx',
+        desc: '222',
+        updatedAt: '2222',
+        createdAt: '3333'
+      }, {
+        id: 10,
+        tag: 'a',
+        title: 'xxx',
+        desc: '222',
+        updatedAt: '2222',
+        createdAt: '3333'
+      }, {
+        id: 11,
+        tag: 'a',
+        title: 'xxx',
+        desc: '222',
+        updatedAt: '2222',
+        createdAt: '3333'
+      }, {
+        id: 12,
+        tag: 'a',
+        title: 'xxx',
+        desc: '222',
+        updatedAt: '2222',
+        createdAt: '3333'
+      }, {
+        id: 13,
+        tag: 'a',
+        title: 'xxx',
+        desc: '222',
+        updatedAt: '2222',
+        createdAt: '3333'
+      }])
+      hasMore.current = res.data.data.hasMore
     })
-  }, [])
+    
+  }, [pageIndex])
 
-  return [list, setList]
+  return [list, hasMore]
 }
 
 export default useMountData
--- src/views/Articles/Articles.tsx
+++ src/views/Articles/Articles.tsx
@@ -2,26 +2,46 @@ import './articles.scss'
 import Article from './components/Item'
 import useMountData from '../../hooks/useMountData'
 import { ArticleType } from './types'
+import { useEffect, useState, useRef } from 'react'
 
 function Articles () {
+  const [pageIndex, setPageIndex] = useState(0)
+  const [list, hasMore] = useMountData(pageIndex) as [ArticleType[], React.MutableRefObject<boolean>]
+  const scrollTopValue = useRef(0)
+  const scrollCb = (e: any) => {
+    if (hasMore.current && e.target.scrollTop - scrollTopValue.current > 400) {
+      scrollTopValue.current = e.target.scrollTop
+      setPageIndex(pageIndex + 1)
+    }
+  }
+  useEffect(() => {
+    let containerRef = document.querySelector('.container')
+    containerRef!.addEventListener('scroll', scrollCb)
+    
+    return () => {
+      containerRef!.removeEventListener('scroll', scrollCb)
+      containerRef = null
+    }
+  }, [])
 
-  const [list, setList] = useMountData()
   return (
-    <div className="content articles">
-      { 
-        (list as ArticleType[]).map
-          (article =>
-            <Article
-              key={article.id}
-              title={article.title}
-              desc={article.desc}
-              createdAt={article.createdAt}
-              tag={article.tag}
-              id={article.id}
-            />
-          )
-      }
-      
+    <div className='content'>
+      <div>{JSON.stringify(hasMore)}</div>
+      <div className="articles">
+        { 
+          (list as ArticleType[]).map
+            (article =>
+              <Article
+                key={article.id}
+                title={article.title}
+                desc={article.desc}
+                createdAt={article.createdAt}
+                tag={article.tag}
+                id={article.id}
+              />
+            )
+        }
+      </div>
     </div>
   )
 }
