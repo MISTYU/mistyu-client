fc642e5076fb1cbb36697600740a840438309549
li chenhui <lichenhui821@qq.com>
2022-12-21 18:43:41 +0800

    feat: icp 备案

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -2,10 +2,13 @@ import icp from '../assets/icons/icp.svg'
 import copyright from '../assets/icons/copyright.svg'
 
 function Footer () {
+  const toIcp = () => {
+    window.open('https://beian.miit.gov.cn', 'blank')
+  }
   return (
     <div className="footer">
       <div className="footer-container">
-        <div className="icp">
+        <div className="icp" onClick={toIcp}>
           <div>
             <img className="icon" src={icp} alt="" />
           </div>
